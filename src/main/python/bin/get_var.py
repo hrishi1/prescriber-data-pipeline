51dbcfe1b6e279765516ddf01a5644256421f5f0
hrishikesh-pawar <114782182+hrishikesh-pawar@users.noreply.github.com>
2022-11-25 23:51:14 -0500

    Declaring variables

---
+++ src/main/python/bin/get_var.py
@@ -0,0 +1,24 @@
+import os
+
+###Set environment variables
+
+#Indicates whether its a test environment or a production environment
+os.environ['envn'] = 'TEST'
+
+#Indicates whether the data to be loaded in spark has headers or not
+os.environ['header'] = 'True'
+
+#Indicates whether the schema in data should be inferred or not
+os.environ['inferSchema'] = 'True'
+
+###Get environment variables
+envn = os.environ['envn']
+header = os.environ['header']
+inferSchema = os.environ['inferSchema']
+
+###Set other variables
+appName = 'Prescriber Data Pipeline'
+current_path = os.getcwd()
+
+staging_dim_city = current_path + '\staging\dimension_city'
+staging_fact = current_path + '\staging\\fact'
--- src/main/python/bin/run_pipeline.py
+++ src/main/python/bin/run_pipeline.py
@@ -1,10 +1,18 @@
-from pyspark.sql import SparkSession
+import get_var as gav
+from create_objects import get_spark_object
+from validations import get_curr_date
 
-spark = SparkSession \
-    .builder \
-    .master('local') \
-    .appName('Testing') \
-    .getOrCreate()
 
-print("Spark Object is Created")
-print(spark)
+def main():
+
+    #Create spark object
+    spark = get_spark_object(gav.envn, gav.appName)
+    print("Spark object is created...")
+
+    #Validate spark object
+    get_curr_date(spark)
+
+
+
+if __name__ == "__main__":
+    main()
